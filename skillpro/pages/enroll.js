2ad5f7f4bc86e69bc39aa21cb5f414e7ce395ccd
Larry <104570219+Larry-Master@users.noreply.github.com>
2025-07-10 12:58:20 +0200

    Update enroll.js

--- skillpro/pages/enroll.js
+++ skillpro/pages/enroll.js
@@ -34,41 +34,6 @@ export default function EnrollPage({ courses }) {
   );
 }
 
-import Head from 'next/head';
-import '@/styles/global.css';
-
-export default function EnrollPage({ courses }) {
-  function enroll(courseTitle) {
-    alert(`You have enrolled in ${courseTitle}!`);
-  }
-
-  return (
-    <>
-      <Head>
-        <title>Course Enrollment</title>
-      </Head>
-      <main>
-        <h1>Welcome to the Enrollment Page!</h1>
-
-        <p>Choose which course to enroll:</p>
-
-        <ul>
-          {courses.map((course) => (
-            <li key={course._id}>
-              {course.title}{' '}
-              <button onClick={() => enroll(course.title)}>Enroll</button>
-            </li>
-          ))}
-        </ul>
-
-        <p className="signature">
-          Signed,<br />
-          HTW Berlin 🦄
-        </p>
-      </main>
-    </>
-  );
-}
 
 export async function getServerSideProps(context) {
   const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
