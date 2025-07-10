4b936404fdc9cbf7ee373a943e9518a59228d0b6
Larry <104570219+Larry-Master@users.noreply.github.com>
2025-07-10 12:41:44 +0200

    Test

--- skillpro/pages/enroll.js
+++ skillpro/pages/enroll.js
@@ -34,9 +34,45 @@ export default function EnrollPage({ courses }) {
   );
 }
 
+import Head from 'next/head';
+import '@/styles/global.css';
+
+export default function EnrollPage({ courses }) {
+  function enroll(courseTitle) {
+    alert(`You have enrolled in ${courseTitle}!`);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Course Enrollment</title>
+      </Head>
+      <main>
+        <h1>Welcome to the Enrollment Page!</h1>
+
+        <p>Choose which course to enroll:</p>
+
+        <ul>
+          {courses.map((course) => (
+            <li key={course._id}>
+              {course.title}{' '}
+              <button onClick={() => enroll(course.title)}>Enroll</button>
+            </li>
+          ))}
+        </ul>
+
+        <p className="signature">
+          Signed,<br />
+          HTW Berlin 🦄
+        </p>
+      </main>
+    </>
+  );
+}
+
 export async function getServerSideProps(context) {
   const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
-  const host = context.req.headers.host; // e.g. localhost:3000 or your deployed domain
+  const host = context.req.headers.host; // localhost:3000 or deployed domain
   const baseUrl = `${protocol}://${host}`;
 
   const res = await fetch(`${baseUrl}/api/courses`);
