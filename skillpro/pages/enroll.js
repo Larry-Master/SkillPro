4c6a9df8fed2efbcd1fa99dec276ee73dcf21a2d
Larry <104570219+Larry-Master@users.noreply.github.com>
2025-07-24 12:17:02 +0200

    Globals CSS

--- skillpro/pages/enroll.js
+++ skillpro/pages/enroll.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import "../styles/globals.css";
 
 export default function EnrollPage({ courses }) {
   function enroll(courseTitle) {
--- skillpro/pages/register.js
+++ skillpro/pages/register.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import "../styles/globals.css";
 
 export default function RegisterForm({ title = "Register" }) {
   return (
