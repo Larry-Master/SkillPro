d45e838eb051863761e32f7f5ad41af70d81f4e5
Larry <104570219+Larry-Master@users.noreply.github.com>
2025-07-24 12:06:58 +0200

    Config Update

--- skillpro/next.config.js
+++ skillpro/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     config.resolve.alias = {
       ...config.resolve.alias,
-      // your aliases
+      // your aliases are automatically handled by Next.js
     };
 
     // Important: return the modified config
